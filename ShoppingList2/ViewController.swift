7687b3e13f111248e86de69b7730ff5f856cb6cc
Claudia Dinu <Claudia.Dinu@ibm.com>
2022-06-20 17:48:30 +0300

    Initial Commit

---
+++ ShoppingList2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShoppingList2
+//
+//  Created by Claudia Dinu on 20.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
